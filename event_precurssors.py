41a482d3bea7fa29cac4fd23bc3a552e90ed3ace
Paulo Ricardo <pauloricardo@pop-os.localdomain>
2021-05-05 14:46:41 -0300

    modifications to the event precursor dataframe

--- event_precurssors.py
+++ event_precurssors.py
@@ -6,6 +6,7 @@ from textwrap import wrap
 import pickle5 as pickle
 from scipy.spatial.distance import cosine
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 df = pd.read_csv('/media/pauloricardo/basement/projeto/df11-03.csv').drop(columns='Unnamed: 0').reset_index(drop=True)
 data = pd.read_csv('/media/pauloricardo/basement/projeto/data11-03.csv').drop(columns='Unnamed: 0').reset_index(drop=True)
@@ -16,25 +17,29 @@ path = "/media/pauloricardo/basement/projeto/"
 for target in targets:
     with open(path + "graphs/graph_" + str(target) + ".gpickle", "rb") as fh:
         G = pickle.load(fh)
-    precursor = {'date': [], 'date_sum': [], 'cos': []}
-    for node in G.nodes:
+    precursor = {'event': [], 'date': [], 'week': [], 'year': [], 'cos': []}
+    for node in tqdm(G.nodes):
         if G.nodes[node]['node_type'] == 'event':
             for neighbor in G.neighbors(node):
                 if G.nodes[neighbor]['node_type'] == 'date':
+                    precursor['event'].append(node)
                     precursor['date'].append(neighbor)
                     neighbor_split = neighbor.split('-')
-                    precursor['date_sum'].append(int(neighbor_split[0]) + int(neighbor_split[1]))
+                    precursor['week'].append(int(neighbor_split[0]))
+                    precursor['year'].append(int(neighbor_split[1]))
                     precursor['cos'].append(1 - cosine(G.nodes[node]['embedding'], data.iloc[target]))
     precursor = pd.DataFrame(precursor)
-    precursor = precursor.groupby(by=['date', 'date_sum'], as_index=False).sum().sort_values(by=['date_sum']).reset_index(drop=True)
+    precursor = precursor.sort_values(by=['year', 'week']).reset_index(drop=True)
+    precursor.to_csv(path + "/precursors/" + str(target) + ".csv")
     print(precursor)
+    
     plt.figure(target)
     g = sns.lineplot(x="date", y="cos",
              data=precursor)
-    g.set_title("\n".join(wrap('event: ' + BeautifulSoup(df['text'].iloc[target], 'html.parser').get_text(), 100)), fontsize=18)
+    g.set_title("\n".join(wrap('event: ' + BeautifulSoup(df['text'].iloc[target], 'html.parser').get_text(), 80)), fontsize=18)
     g.set_xlabel('week-year', fontsize=14)
-    g.set_ylabel('accmulated cosine', fontsize=14)
+    g.set_ylabel('cosine', fontsize=14)
     for item in g.get_xticklabels():
         item.set_rotation(45)
     g.get_figure().set_size_inches(12,8)
-    g.get_figure().savefig(path + "/precursors/" + str(target) + ".pdf")    
+    g.get_figure().savefig(path + "/precursors/" + str(target) + ".pdf") 
