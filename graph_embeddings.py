9ae2ba227ab006d03fdf06ec1d70b4229af4d8da
Paulo Ricardo <pauloricardo@pop-os.localdomain>
2021-05-07 17:32:41 -0300

    dynamic vram allocation

--- graph_embeddings.py
+++ graph_embeddings.py
@@ -14,6 +14,9 @@ import matplotlib.pyplot as plt
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 from ge import DeepWalk
 from ge import Node2Vec
 from ge import Struc2Vec
