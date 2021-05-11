05df8af870221c51b76328880719130efb57910c
Paulo Ricardo <pauloricardo@pop-os.localdomain>
2021-05-11 17:35:53 -0300

    acc as metric type

--- graph_evaluation.py
+++ graph_evaluation.py
@@ -13,7 +13,7 @@ all_files = glob.glob(os.path.join(path, "*.csv"))
 targets = [377904, 375777,  380274, 377199, 389118, 389293, 388224, 397968, 394909, 394491, 372939, 402610, 380994]
 algorithms = ['regularization', 'deep_walk', 'node2vec', 'line', 'struct2vec', 'gcn']
 splits = [0.05, 0.1, 0.15, 0.2]
-metrics = ['recall']
+metrics = ['acc']
 edge_types = ['event_location', 'event_actor']
 
 results_df = {'metric': [], 'algorithm': [], 'target': [], 'split': [], 'type': [], 'value': []}
